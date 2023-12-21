47a9963ee4192ad389c3edac5e008a6892728808
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-12-22 00:29:06 +0500

    Add Student form kar kam kiya

---
+++ src/Components/AddStudent/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./AddStudent.css"
+
+function AddStudentForm() {
+  return (
+    <div className='addStudentform'>
+        {/* <img src="https://img.freepik.com/premium-vector/cute-smiling-boy-avatar-flat-style-vector-illustration_710508-1241.jpg?w=2000" alt="" /> */}
+    </div>
+  )
+}
+
+export default AddStudentForm
--- src/pages/AddStudent/index.jsx
+++ src/pages/AddStudent/index.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import './AddStudent.css'
+import React from 'react';
+import './AddStudent.css';
+
+import Sidebar from "../../Components/Sidebar/index.jsx";
+import Navbar from "../../Components/Navbar/index.jsx";
+import AddStudentForm from "../../Components/AddStudent/index.jsx";
 
 function AddStudent() {
   return (
-    <div>AddStudent</div>
+    <div className="d-flex AddStudentWrapper">
+      <Sidebar currentPage={"Add Student"} />
+      <div className='navbarofAddStudent'>
+        <Navbar />
+        <h2 className='text-center' style={{ color: "#0873b9" }}>Add Student</h2>
+        <AddStudentForm />
+      </div>
+    </div>
   )
 }
 

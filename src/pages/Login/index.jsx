c5cd037e0ab2145135e10532368ba728739c04e9
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-12-22 18:35:48 +0500

    Login ka page ban gya

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,7 +3,19 @@ import './Login.css'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='loginwrapper'>
+      <div className='logincard'>
+        <h1>Login</h1>
+        <label className="col-form-label txtoflogin">Write admin email here:</label>
+        <div className="input-group">
+          <span className="input-group-text" id="basic-addon1">@</span>
+          <input type="text" className="form-control inputoflogin" placeholder="Admin Email" aria-label="Username" aria-describedby="basic-addon1" />
+        </div>
+        <label className="col-form-label txtoflogin">Write admin password here:</label>
+        <input type="password" placeholder='Admin Password' className="form-control inputoflogin" id="inputPassword" />
+        <button type="button" className="btn btn-primary mt-3">Login</button>
+      </div>
+    </div>
   )
 }
 

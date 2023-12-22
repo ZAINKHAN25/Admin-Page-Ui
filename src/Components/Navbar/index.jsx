62444065b1871bd512f3553f871e5dc9a9e1bcff
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-12-22 12:57:30 +0500

    Update admin page bhi hogya

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
-import "./navbar.css";
+import './navbar.css';
+import { useNavigate } from 'react-router-dom';
 
-function Navbar() {
+function Navbar({ home }) {
+    const navigateTo = useNavigate();
+
     return (
         <div className='d-flex navbarwrapper'>
-            <div className='fw-bold fs-5'>Hello, Zain👋</div>
+            <div className='fw-bold fs-5'>
+                {home === true ? (
+                    <span onClick={() => navigateTo('/')}> {true && "<"} Home</span>
+                ) : (
+                    'Hello, Zain👋'
+                )}
+            </div>
             <div className='inputareadiv'>
                 <div className='inputdivnavbar'>
-                    <input type="text" className='inputofnavbar' placeholder='Search a Student' />
-                    <i className="fa-solid fa-magnifying-glass"></i>
+                    <input
+                        type='text'
+                        className='inputofnavbar'
+                        placeholder='Search a Student'
+                        id='searchInput'
+                    />
+                    <i className='fa-solid fa-magnifying-glass'></i>
                 </div>
-                <img className='imgofnavbar' src="https://img.freepik.com/premium-vector/cute-smiling-boy-avatar-flat-style-vector-illustration_710508-1241.jpg?w=2000" alt="" />
+                <img
+                    className='imgofnavbar'
+                    src='https://img.freepik.com/premium-vector/cute-smiling-boy-avatar-flat-style-vector-illustration_710508-1241.jpg?w=2000'
+                    alt='User Avatar'
+                    onClick={ () => navigateTo("/admin-profile")}
+                    title='Profile page'
+                />
             </div>
         </div>
-    )
+    );
 }
 
 export default Navbar;

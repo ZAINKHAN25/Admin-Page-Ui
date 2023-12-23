71d18339bdaeca87a949534d617c3a2782382188
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-12-23 11:48:19 +0500

    All Students me thora aur kam kya

--- src/Components/AllStudents/index.jsx
+++ src/Components/AllStudents/index.jsx
@@ -7,18 +7,18 @@ function AllStudents() {
     return (
         <div className='AllStudentsWrapper m-5 p-5'>
             {/* modal */}
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Zain's Data</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="staticBackdropLabel">Zain's Data</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
+                        <div className="modal-body">
                             <div style={{ width: "100%" }}>
                                 Student Img:
                                 <div style={{ width: "100%", display: "flex", justifyContent: 'center' }}>
-                                    <img src="https://img.freepik.com/premium-vector/cute-smiling-boy-avatar-flat-style-vector-illustration_710508-1241.jpg?w=2000" alt="" className='seestudentmodalimg' srcset="" />
+                                    <img src="https://img.freepik.com/premium-vector/cute-smiling-boy-avatar-flat-style-vector-illustration_710508-1241.jpg?w=2000" alt="" className='seestudentmodalimg' />
                                 </div>
                             </div>
                             <div>Student Name: <b>Zain</b></div>
@@ -26,8 +26,8 @@ function AllStudents() {
                             <div>Student Course: <b>Web and Mobile</b></div>
                             <div>Student email: <b>zainarfeen87@gmail.com</b></div>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         </div>
                     </div>
                 </div>
